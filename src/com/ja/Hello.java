d678892abb85fd1372d73ab7f38d40cfdefaf746
kuccho524 <kucchachachanocha@gmail.com>
2021-08-16 21:06:21 +0900

    Initial Commit

---
+++ src/com/ja/Hello.java
@@ -0,0 +1,13 @@
+package com.ja;
+
+/**
+ * 「hello java」を表示させる
+ * @author kuccho524
+ *
+ */
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
+}
---
+++ src/com/ja/javapractoce.java
@@ -0,0 +1,5 @@
+package com.ja;
+
+public class javapractoce {
+
+}
